f26acb13542f4ca4f827ac2335bed9702d12d372
Juan Gonzalez <jdgonzalez2017@hotmail.com>
2023-10-10 23:23:27 -0300

    Modificando rutas de imagenes

--- imagesLashess/imagesLashess.js
+++ imagesLashess/imagesLashess.js
@@ -1,22 +1,22 @@
 const imagesLashess = [
   [
-    { src: "public/pestañas-cejas2.jpeg", name: "pestañas-cejas2" },
-    { src: "public/pestañas7.jpeg", name: "pestañas7" },
-    { src: "public/pestañas4.jpeg", name: "pestañas4" },
-    { src: "public/pestañas1.jpeg", name: "pestañas1" },
+    { src: "pestañas-cejas2.jpeg", name: "pestañas-cejas2" },
+    { src: "pestañas7.jpeg", name: "pestañas7" },
+    { src: "pestañas4.jpeg", name: "pestañas4" },
+    { src: "pestañas1.jpeg", name: "pestañas1" },
   ],
   [
-    { src: "public/pestañas3.jpeg", name: "pestañas3" },
-    { src: "public/lifting1.jpeg", name: "lifting1" },
-    { src: "public/pestañas-cejas1.jpeg", name: "pestañas-cejas1" },
-    { src: "public/depilacion3.jpeg", name: "depilacion3" },
+    { src: "pestañas3.jpeg", name: "pestañas3" },
+    { src: "lifting1.jpeg", name: "lifting1" },
+    { src: "pestañas-cejas1.jpeg", name: "pestañas-cejas1" },
+    { src: "depilacion3.jpeg", name: "depilacion3" },
   ],
 
   [
-    { src: "public/lifting2.jpeg", name: "lifting2" },
-    { src: "public/pestañas5.jpeg", name: "pestañas5" },
-    { src: "public/pestañas6.jpeg", name: "pestañas6" },
-    { src: "public/pestañas2.jpeg", name: "pestañas2" },
+    { src: "lifting2.jpeg", name: "lifting2" },
+    { src: "pestañas5.jpeg", name: "pestañas5" },
+    { src: "pestañas6.jpeg", name: "pestañas6" },
+    { src: "pestañas2.jpeg", name: "pestañas2" },
   ],
 ];
 
