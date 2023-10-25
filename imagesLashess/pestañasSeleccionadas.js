665a4af368e71dbfd90a6e14b8331df4d0896861
Juan Gonzalez <jdgonzalez2017@hotmail.com>
2023-10-24 22:26:44 -0300

    Actualizando precios de pestañas

--- imagesLashess/pestañasSeleccionadas.js
+++ imagesLashess/pestañasSeleccionadas.js
@@ -2,7 +2,7 @@ const pestañasSeleccionadas = [
   {
     name: "Volúmen Brasilero",
     src: "pestañas6.jpeg",
-    price: 5000,
+    price: 6000,
     description:
       "Las pestañas de volumen brasileño son una técnica de extensión de pestañas originaria de Brasil. Se caracteriza por aplicar múltiples extensiones en forma de abanico en una sola pestaña natural, creando un efecto de mayor densidad y volumen. Se utilizan pestañas delgadas y livianas para un aspecto natural. Requiere habilidad y experiencia del estilista y es conocida por realzar la belleza de los ojos. Es una opción popular para obtener una mirada impactante y duradera.",
       id:1
@@ -11,7 +11,7 @@ const pestañasSeleccionadas = [
   {
     name: "Efecto Rímel",
     src: "pestañas2.jpeg",
-    price: 4500,
+    price: 5500,
     description:
       "En este procedimiento, se aplican extensiones individuales a las pestañas naturales de manera similar a las extensiones clásicas, pero con un enfoque en lograr un efecto de máscara de pestañas tipo rimel. Esto significa que las extensiones se seleccionan y se aplican de manera que imiten el aspecto de pestañas recién maquilladas con máscara de pestañas, con un aspecto más oscuro, largo y voluminoso.",
       id:2
@@ -19,7 +19,7 @@ const pestañasSeleccionadas = [
   {
     name: "Clásicas",
     src: "pestañas4-copia.jpeg",
-    price: 4000,
+    price: 5000,
     description:
       "Las extensiones de pestañas clásicas son una técnica de belleza que implica la adhesión de extensiones individuales a las pestañas naturales. Estas extensiones se aplican una por una para aumentar la longitud y la plenitud de las pestañas. El resultado es una apariencia más definida y elegante que realza la mirada. A diferencia del volumen brasileño, las extensiones clásicas tienden a ser más sutiles y naturales.",
       id:3
@@ -27,7 +27,7 @@ const pestañasSeleccionadas = [
   {
     name: "Lifting + Keratina + Color",
     src: "lifting2.jpeg",
-    price: 3700,
+    price: 4300,
     description:
       "El lifting de pestañas es un procedimiento de belleza que busca realzar y levantar las pestañas naturales de manera temporal. Este tratamiento implica el uso de productos químicos suaves para doblar y elevar las pestañas desde la raíz, creando una apariencia más abierta y despierta en los ojos. ",
       id:4
@@ -35,7 +35,7 @@ const pestañasSeleccionadas = [
   {
     name: "Perfilado de cejas",
     src: "pestañas-cejas1.jpeg",
-    price: 3800,
+    price: 3900,
     description:
       "El perfeccionamiento de cejas es un proceso de embellecimiento que tiene como objetivo mejorar la forma, el diseño y la apariencia de las cejas naturales. Esto se logra mediante técnicas como el diseño de cejas, la depilación con cera, el microblading o la aplicación de tintes. El resultado es un marco facial más armonioso y definido, que resalta los rasgos faciales.",
       id:5
