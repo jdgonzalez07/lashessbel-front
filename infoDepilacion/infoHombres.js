91a01a6bd6ca3db8422527c1bb0ea7ca4b326c74
Juan Gonzalez <jdgonzalez2017@hotmail.com>
2023-11-06 19:01:17 -0300

    actualizando precios de depilación y error de rutas de vercel

--- infoDepilacion/infoHombres.js
+++ infoDepilacion/infoHombres.js
@@ -2,58 +2,63 @@ const infoHombres = [
   {
     title: "Combo 1",
     zones: "Pecho + Abdomen",
-    price: 2700,
+    price: 2800,
   },
   {
     title: "Combo 2",
     zones: "Pecho + Espalda",
-    price: 2400,
+    price: 2500,
   },
   {
     title: "Combo 3",
     zones: "Espalda + Abdomen",
-    price: 2400,
+    price: 2500,
   },
   {
     title: "Combo 4",
     zones: "Pecho + Abdomen + Hombros ó Axilas",
-    price: 3200,
+    price: 3500,
   },
   {
     title: "Combo 5",
     zones: "Pierna entera + Glúteos",
-    price: 3400,
+    price: 3600,
   },
   {
     title: "Combo 6",
     zones: "Espalda + Pecho + Abdomen",
-    price: 3400,
+    price: 3600,
   },
   {
     title: "Combo 7",
     zones: "Brazo entero + Axilas",
-    price: 2800,
+    price: 3000,
   },
   {
     title: "Combo 8",
     zones: "Pierna entera + Pecho + Abdomen",
-    price: 3500,
+    price: 3800,
   },
   {
     title: "Combo 9",
     zones: "Espalda + Pecho + Abdomen + Pierna entera",
-    price: 2700,
+    price: 4500,
   },
   {
     title: "Combo 10",
+    zones: "Pecho + Abdomen + Axilas + Piernas enteras",
+    price: 4500,
+  },
+  {
+    title: "Libre",
     zones: "Una zona a elección",
     price: 2000,
   },
   {
-    title: "Combo 11",
-    zones: "Más de cuatro zonas a elección",
-    price: 2700,
-  },
+    title: "Libre",
+    zones: "Cinco zonas a elección",
+    price: 5000,
+  }
 
 ];
 
--- infoDepilacion/infoMujeres.js
+++ infoDepilacion/infoMujeres.js
@@ -1,85 +1,111 @@
 const infoMujeres = [
   {
+    title: "Cuerpo entero",
+    zones:
+      "Cavado completo + Tira de cola + Axilas + Pierna entera + Bozo + Mentón + Brazos + Glúteos",
+    price: 5500,
+  },
+
+  {
     title: "Combo 1",
     zones: "Cavado completo + Tira de cola",
-    price: 2100,
+    price: 2300,
   },
   {
     title: "Combo 2",
     zones: "Cavado completo + Axila",
-    price: 2100,
+    price: 2300,
   },
   {
     title: "Combo 3",
     zones: "Pierna entera + Cavado completo + Axila",
-    price: 2500,
+    price: 2700,
   },
   {
     title: "Combo 4",
     zones: "Pierna entera + Axila + Cavado completo + Tira de cola",
-    price: 2800,
+    price: 3000,
   },
   {
     title: "Combo 5",
     zones: "Cavado completo + Tira de cola + Axila",
-    price: 2400,
+    price: 2600,
   },
   {
     title: "Combo 6",
     zones: "Bozo + Mentón + Axila",
-    price: 2400,
+    price: 2500,
   },
   {
     title: "Combo 7",
     zones: "Bozo + Axila",
-    price: 2100,
+    price: 2300,
   },
   {
     title: "Combo 8",
     zones: "Media pierna + Axila",
-    price: 2100,
+    price: 2300,
   },
   {
     title: "Combo 9",
     zones: "Media pierna + Bozo",
-    price: 2100,
-  },
-  {
-    title: "Combo 10",
-    zones: "Más de cuatro zonas a elección",
-    price: 4000,
-  },
-  {
-    title: "Combo 11",
-    zones: "Una zona a elección",
-    price: 1500,
-  },
-  {
-    title: "Combo 12",
-    zones: "Bozo + mentón",
-    price: 2100,
-  },
-  {
-    title: "Combo 13",
-    zones: "Pierna entera + Axila",
-    price: 2200,
-  },
-  {
-    title: "Combo 14",
-    zones: "Rostro completo",
     price: 2300,
   },
   {
-    title: "Combo 15",
-    zones: "Rostro completo + Axila",
-    price: 2500,
+    title: "Combo 10",
+    zones: "Bozo + Mentón",
+    price: 2300,
   },
   {
-    title: "Combo 16",
-    zones: "Brazos + Axila + Cavado completo",
-    price: 2700,
+    title: "Combo 11",
+    zones: "Pierna entera + Axila",
+    price: 2300,
   },
-  
+  {
+    title: "Combo 12",
+    zones: "Rostro completo",
+    price: 2400,
+  },
+  {
+    title: "Combo 13",
+    zones: "Rostro completo + Axila",
+    price: 2600,
+  },
+  {
+    title: "Combo 14",
+    zones: "Brazos + Axila + Cavado completo",
+    price: 2900,
+  },
+  {
+    title: "Libre",
+    zones: "Una zona a elección",
+    price: 1700,
+  },
+  {
+    title: "Libre",
+    zones: "Dos zoinas a elección",
+    price: 3000,
+  },
+  {
+    title: "Libre",
+    zones: "Tres zonas a elección",
+    price: 3500,
+  },
+  {
+    title: "Libre",
+    zones: "Cuatro zonas a elección",
+    price: 4000,
+  },
+  {
+    title: "Libre",
+    zones: "Seis zonas a elección",
+    price: 5000,
+  },
+  {
+    title: "Libre",
+    zones: "Ocho zonas a elección",
+    price: 6000,
+  },
 ];
 
 export default infoMujeres;
