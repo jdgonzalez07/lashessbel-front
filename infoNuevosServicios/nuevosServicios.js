775b8587306c8dec135058810910287c6d4c064c
Juan Gonzalez <jdgonzalez2017@hotmail.com>
2023-12-07 07:42:26 -0300

    agregando archivos de la página nueva

--- infoNuevosServicios/nuevosServicios.js
+++ infoNuevosServicios/nuevosServicios.js
@@ -5,10 +5,12 @@ const nuevosServicios = [
       "Es un tratamiento no invasivo que gracias a la combinación de 4 técnicas nos permite obtener grandes beneficios.",
       "Radiofrecuencia: Estimula la formación de colágeno.",
       "Láser Infrarrojo: Reduce la adiposidad y modela el contorna corporal.",
-      "Vacumterapia: Elimina la celulitis y mejora la circulación.",
-      "Masaje de rodillos: Modela el contorno de la zona tratada y favorece el drenaje linfático.",
+      /* "Vacumterapia: Elimina la celulitis y mejora la circulación.",
+      "Masaje de rodillos: Modela el contorno de la zona tratada y favorece el drenaje linfático.", */
     ],
-    price: 2500,
+    price: "$2500 por zona",
+    zones:["Abdomen", "Flancos", "Isquiotibiales", "Cuadriceps", "Glúteos"],
+    src:"vela2.jpeg"
   },
   {
     title: "Body Up",
@@ -16,9 +18,11 @@ const nuevosServicios = [
       "︎Reafirma y levanta glúteos, abdomen, muslos [puede ser isquiotibiales, cuadriceps o aductores] y brazos",
       "Va dirigido a todas aquellas personas que necesitan tonificación muscular y reafirmación de la piel en una zona localizada. ",
     ],
-    price: 2000,
+    price: "$2000 por zona",
+    zones:["Abdomen", "Muslos", "Brazos", "Cuadriceps", "Glúteos"],
+    src:"body1.jpeg"
   },
-  {
+  /* {
     title: "Criolipolisis Plana",
     description: [
       "︎Sirve para tratar el abdomen, flancos, brazos, isquiotibiales, cuadriceps.",
@@ -27,7 +31,7 @@ const nuevosServicios = [
       "Es ideal para la grasita localizada"
     ],
     price: 0,
-  },
+  }, */
 ];
 
 export default nuevosServicios;
